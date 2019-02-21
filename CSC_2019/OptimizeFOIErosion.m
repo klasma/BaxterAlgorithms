4fe2a9f9d86d278b6853c6bef262a27f66f9c48c
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2019-02-21 17:36:06 +0100

    Continued to work on FOI erosion optimization.
    Signed-off-by: Klas Magnusson <klas.magnusson@raysearchlabs.com>

--- CSC_2019/OptimizeFOIErosion.m
+++ CSC_2019/OptimizeFOIErosion.m
@@ -1,34 +1,45 @@
-seqPath = 'C:\Users\klasma\Dropbox\DataSets\Fluo-C2DL-MSC\Fluo-C2DL-MSC_01';
-imData = ImageData(seqPath);
-version = '_190220_223623_no_foi_erosion';
-maxErosion = 100;
-SEG = nan(maxErosion + 1, 1);
-TRA = nan(maxErosion + 1, 1);
+function OptimizeFOIErosion(aSeqPaths, aVersion)
 
-for erosion = 0:maxErosion
-    fprintf('Testing FOI erosion %d / %d\n', erosion, maxErosion)
-    cells = LoadCells(seqPath, version);
-    erodedVersion = sprintf('%s_eroded_%03d', version, erosion);
-    if ~HasVersion(seqPath, erodedVersion)
-        erodedCells = ErodeFOI(cells, erosion, imData);
-        SaveCells(erodedCells, seqPath, erodedVersion)
+maxErosion = 100;
+SEG = nan(maxErosion + 1, length(aSeqPaths));
+TRA = nan(maxErosion + 1, length(aSeqPaths));
+
+for i = 1:length(aSeqPaths)
+    seqPath = aSeqPaths{i};
+    imData = ImageData(seqPath);
+    for erosion = 0:maxErosion
+        fprintf('Testing FOI erosion %d / %d\n', erosion, maxErosion)
+        cells = LoadCells(seqPath, aVersion);
+        erodedVersion = sprintf('%s_eroded_%03d', aVersion, erosion);
+        if ~HasVersion(seqPath, erodedVersion)
+            erodedCells = ErodeFOI(cells, erosion, imData);
+            SaveCells(erodedCells, seqPath, erodedVersion)
+        end
+        SEG(erosion+1) = PerformanceSEG(seqPath, erodedVersion, false);
+        TRA(erosion+1) = PerformanceTRA(seqPath, erodedVersion);
     end
-    SEG(erosion+1) = PerformanceSEG(seqPath, erodedVersion, false);
-    TRA(erosion+1) = PerformanceTRA(seqPath, erodedVersion);
 end
 
-OP = (TRA+SEG)/2;
+meanTRA = mean(TRA,2);
+meanSEG = mean(SEG,2);
 
-bestErosion = find(OP == max(OP), 1, 'last') - 1;
+meanOP = (meanTRA+meanSEG)/2;
+
+bestErosion = find(meanOP == max(meanOP), 1, 'last') - 1;
 fprintf('The best erosion is %d\n', bestErosion)
 
+for i = 1:length(aSeqPaths)
+    WriteSeqSettings(aSeqPaths{i}, 'foiErosion', bestErosion)
+end
+
 figure
-plot(0:maxErosion, SEG)
+plot(0:maxErosion, meanSEG)
 hold all
-plot(0:maxErosion, TRA)
-plot(0:maxErosion, OP)
+plot(0:maxErosion, meanTRA)
+plot(0:maxErosion, meanOP)
 
 xlabel('Erosion')
 ylabel('Performance')
 legend('SEG', 'TRA', 'OP')
 grid on
+end
