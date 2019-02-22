4139a17928223c989cd8cb4507f98c02e4350bce
Marcus Nordström <marnor@raysearchlabs.com>
2019-02-22 15:28:44 +0100

    Fixed menu order bug (issue #6).
    The menu order in the main GUI changed when the menu visibility was changed
    from basic to advanced. This resolves issue #6.

--- GUIs/Menus/CreateMenus.m
+++ GUIs/Menus/CreateMenus.m
@@ -6,9 +6,11 @@ function oMenus = CreateMenus(aParent, aDefs)
 % levels of the menus ('basic', 'advanced' or 'development'), and the third
 % column contains function handles with callbacks for the menus. The second
 % column is not used to generate the menus and can therefore be left empty.
-% Sub-menus are created by replacing the callback by a Mx3 cell array
-% representing M sub-menus. Sub-menus can be nested in in multiple layers.
-% In this function, the nesting is achieved through recursion.
+% Sub-menus are created by replacing the callback by an Mx3 cell array
+% representing M sub-menus. Sub-menus can be nested in multiple layers. In
+% this function, the nesting is achieved through recursion. The menus are
+% all created with visibility set to 'off', because otherwise the menus can
+% change order when the visibilities are changed using SetVisibleMenus.
 %
 % Inputs:
 % aParent - Figure or uimenu where the menus will be placed.
@@ -27,13 +29,15 @@ for i = 1:size(aDefs,1)
     if iscell(aDefs{i,3})
         % Create sub-menus.
         oMenus{i,4} = uimenu(aParent,...
-            'Label', aDefs{i,1});
+            'Label', aDefs{i,1},...
+            'Visible', 'off');
         oMenus{i,3} = CreateMenus(oMenus{i,4}, aDefs{i,3});
     else
         % Add callback.
         oMenus{i,4} = uimenu(aParent,...
             'Label', aDefs{i,1},...
-            'Callback', aDefs{i,3});
+            'Callback', aDefs{i,3},...
+            'Visible', 'off');
     end
 end
 end
