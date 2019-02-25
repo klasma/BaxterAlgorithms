ed40cd402903b86d373b83dfaf5b9a0337ab9c3d
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2019-02-25 16:51:51 +0100

    Updated the script which selects tracks with a ground truth in frame 1.
    Signed-off-by: Klas Magnusson <klas.magnusson@raysearchlabs.com>

--- CSC_2019/selectTRICGTCells.m
+++ CSC_2019/selectTRICGTCells.m
@@ -3,14 +3,15 @@
 
 basePath = 'D:\CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019';
 seqPaths = {...
+    fullfile(basePath, 'Training', 'Fluo-N3DL-TRIC', 'Fluo-N3DL-TRIC_01')
     fullfile(basePath, 'Training', 'Fluo-N3DL-TRIC', 'Fluo-N3DL-TRIC_02')
     fullfile(basePath, 'Challenge', 'Fluo-N3DL-TRIC', 'Fluo-N3DL-TRIC_01')
     fullfile(basePath, 'Challenge', 'Fluo-N3DL-TRIC', 'Fluo-N3DL-TRIC_02')};
-version = '_190219_173313_initial';
+version = '_hpc';
 
 for i = 1:length(seqPaths)
     selVersion = [version '_sel'];
-    if ~HasVersion(seqPaths{i}, selVersion)
+    if HasVersion(seqPaths{i}, version) && ~HasVersion(seqPaths{i}, selVersion)
         SaveSelectedGTCells(seqPaths{i}, version, selVersion)
     end
 end
