5a6c6ab15ef1742198c3a16170cf2651cf9a191f
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2019-02-26 01:09:34 +0100

    Changed the folder for TRIC segmentation optimization.
    Signed-off-by: Klas Magnusson <klas.magnusson@raysearchlabs.com>

--- CSC_2019/optimizeTRICSegmentation.m
+++ CSC_2019/optimizeTRICSegmentation.m
@@ -6,7 +6,7 @@
 subdirs = textscan(genpath(fileparts(fileparts(mfilename('fullpath')))), '%s','delimiter', pathsep);
 addpath(subdirs{1}{:});
 
-basePath = 'D:\CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training';
+basePath = 'C:\CellData\Training';
 exPath = fullfile(basePath, 'Fluo-N3DL-TRIC');
 
 optimizer = SEGOptimizerEx(exPath,...
