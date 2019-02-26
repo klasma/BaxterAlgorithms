f5c9ce229d5758fbd1046621053870695deeb2c0
Klas Magnusson <klasmagnus@gmail.com>
2019-02-27 00:11:34 +0100

    Wrote scripts to optimize A549 and A549-SIM segmentation.
    Signed-off-by: Klas Magnusson <klasmagnus@gmail.com>

---
+++ CSC_2019/optimizeA549Segmentation.m
@@ -0,0 +1,15 @@
+
+subdirs = textscan(genpath(fileparts(fileparts(mfilename('fullpath')))), '%s','delimiter', pathsep);
+addpath(subdirs{1}{:});
+
+basePath = 'E:\CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training';
+exPath = fullfile(basePath, 'Fluo-C3DH-A549');
+
+optimizer = SEGOptimizerEx(exPath,...
+    {'SegClipping'
+    'BPSegHighStd'
+    'BPSegLowStd'
+    'BPSegBgFactor'
+    'BPSegThreshold'});
+
+optimizer.Optimize_coordinatedescent('MaxIter', 25)
