85dee331d493978c12d99277113c4aa52e158dcd
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2019-02-27 16:37:14 +0100

    Changed path in segmentation optimization.
    Signed-off-by: Klas Magnusson <klas.magnusson@raysearchlabs.com>

--- CSC_2019/optimizeA549Segmentation.m
+++ CSC_2019/optimizeA549Segmentation.m
@@ -2,7 +2,8 @@
 subdirs = textscan(genpath(fileparts(fileparts(mfilename('fullpath')))), '%s','delimiter', pathsep);
 addpath(subdirs{1}{:});
 
-basePath = 'E:\CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training';
+%basePath = 'E:\CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training';
+basePath = 'C:\CellData\Training';
 exPath = fullfile(basePath, 'Fluo-C3DH-A549');
 
 optimizer = SEGOptimizerEx(exPath,...
