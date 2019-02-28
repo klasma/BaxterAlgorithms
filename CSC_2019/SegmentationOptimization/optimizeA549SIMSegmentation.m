feb6ed23c437d1cdefd60b16e0d50520fbdae6f8
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2019-02-28 11:39:21 +0100

    Updated A549 segmenation optimization to work at work and on laptop.
    Signed-off-by: Klas Magnusson <klas.magnusson@raysearchlabs.com>

--- CSC_2019/SegmentationOptimization/optimizeA549SIMSegmentation.m
+++ CSC_2019/SegmentationOptimization/optimizeA549SIMSegmentation.m
@@ -1,8 +1,10 @@
+% Set SegClipping to 0.25 before starting the optimization.
 
 subdirs = textscan(genpath(fileparts(fileparts(fileparts(mfilename('fullpath'))))), '%s','delimiter', pathsep);
 addpath(subdirs{1}{:});
 
-basePath = 'E:\CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training';
+basePath = fullfile(ExternalDrive(),...
+    'CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training');
 exPath = fullfile(basePath, 'Fluo-C3DH-A549-SIM');
 
 optimizer = SEGOptimizerEx(exPath,...
--- CSC_2019/SegmentationOptimization/optimizeA549Segmentation.m
+++ CSC_2019/SegmentationOptimization/optimizeA549Segmentation.m
@@ -2,8 +2,8 @@
 subdirs = textscan(genpath(fileparts(fileparts(fileparts(mfilename('fullpath'))))), '%s','delimiter', pathsep);
 addpath(subdirs{1}{:});
 
-%basePath = 'E:\CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training';
-basePath = 'C:\CellData\Training';
+basePath = fullfile(ExternalDrive(),...
+    'CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training');
 exPath = fullfile(basePath, 'Fluo-C3DH-A549');
 
 optimizer = SEGOptimizerEx(exPath,...
