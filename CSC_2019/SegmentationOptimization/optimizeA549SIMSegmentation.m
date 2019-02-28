996c8f852e56330f37e3372ccd028c5cefa92c4b
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2019-02-28 15:01:09 +0100

    Separate optimizations for the simulated A5439 sequences.
    Signed-off-by: Klas Magnusson <klas.magnusson@raysearchlabs.com>

--- CSC_2019/SegmentationOptimization/optimizeA549SIMSegmentation.m
+++ CSC_2019/SegmentationOptimization/optimizeA549SIMSegmentation.m
@@ -1,17 +1,21 @@
-% Set SegClipping to 0.25 before starting the optimization.
-
 subdirs = textscan(genpath(fileparts(fileparts(fileparts(mfilename('fullpath'))))), '%s','delimiter', pathsep);
 addpath(subdirs{1}{:});
 
 basePath = fullfile(ExternalDrive(),...
     'CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training');
 exPath = fullfile(basePath, 'Fluo-C3DH-A549-SIM');
+seqDirs = GetSeqDirs(exPath);
 
-optimizer = SEGOptimizerEx(exPath,...
-    {'SegClipping'
-    'BPSegHighStd'
-    'BPSegLowStd'
-    'BPSegBgFactor'
-    'BPSegThreshold'});
-
-optimizer.Optimize_coordinatedescent('MaxIter', 25)
+for i = 1:length(seqDirs)
+    seqPath = fullfile(exPath, seqDirs{i});
+    fprintf('Optimizing seqmentation for %s\n', seqDirs{i})
+    
+    optimizer = SEGOptimizerSeq(seqPath,...
+        {'SegClipping'
+        'BPSegHighStd'
+        'BPSegLowStd'
+        'BPSegBgFactor'
+        'BPSegThreshold'});
+    
+    optimizer.Optimize_coordinatedescent('MaxIter', 25)
+end
