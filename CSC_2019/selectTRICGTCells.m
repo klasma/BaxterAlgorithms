fab5297fe2de7a82e07479f0b28034ecd1521966
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2019-02-28 11:40:10 +0100

    Made track selection script work at work and on laptop and added parfor.
    Signed-off-by: Klas Magnusson <klas.magnusson@raysearchlabs.com>

--- CSC_2019/selectTRICGTCells.m
+++ CSC_2019/selectTRICGTCells.m
@@ -1,7 +1,8 @@
 % Script which creates a tracking version with TRIC cells that have a
 % ground truth marker in the first image.
 
-basePath = 'D:\CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019';
+basePath = fullfile(ExternalDrive(),...
+    'CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019');
 seqPaths = {...
     fullfile(basePath, 'Training', 'Fluo-N3DL-TRIC-cropped', 'Fluo-N3DL-TRIC_01')
     fullfile(basePath, 'Training', 'Fluo-N3DL-TRIC-cropped', 'Fluo-N3DL-TRIC_02')
@@ -9,9 +10,9 @@ seqPaths = {...
     fullfile(basePath, 'Training', 'Fluo-N3DL-TRIC', 'Fluo-N3DL-TRIC_02')
     fullfile(basePath, 'Challenge', 'Fluo-N3DL-TRIC', 'Fluo-N3DL-TRIC_01')
     fullfile(basePath, 'Challenge', 'Fluo-N3DL-TRIC', 'Fluo-N3DL-TRIC_02')};
-version = '_hpc2single';
+version = '_hpc4';
 
-for i = 1:length(seqPaths)
+parfor i = 1:length(seqPaths)
     selVersion = [version '_sel'];
     if HasVersion(seqPaths{i}, version) && ~HasVersion(seqPaths{i}, selVersion)
         SaveSelectedGTCells(seqPaths{i}, version, selVersion)
