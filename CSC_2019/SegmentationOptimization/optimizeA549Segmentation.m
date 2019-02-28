5ba68d38fbeb74315cd2ab5ea187f273bafefc7b
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2019-02-28 18:05:42 +0100

    Separated segmentation optimzation of A549 sequences.
    I also removed tophat filtering and clipping to make the algorithm simpler.
    
    Signed-off-by: Klas Magnusson <klas.magnusson@raysearchlabs.com>

--- CSC_2019/SegmentationOptimization/optimizeA549Segmentation.m
+++ CSC_2019/SegmentationOptimization/optimizeA549Segmentation.m
@@ -1,19 +1,20 @@
-% Set SegTopHatRadius to 3 before starting the optimization.
-
 subdirs = textscan(genpath(fileparts(fileparts(fileparts(mfilename('fullpath'))))), '%s','delimiter', pathsep);
 addpath(subdirs{1}{:});
 
 basePath = fullfile(ExternalDrive(),...
     'CellData_2011_2014\2019_02_14_Cell_Tracking_Challenge_2019\Training');
 exPath = fullfile(basePath, 'Fluo-C3DH-A549');
+seqDirs = GetSeqDirs(exPath);
 
-optimizer = SEGOptimizerEx(exPath,...
-    {'SegClipping'
-    'BPSegHighStd'
-    'BPSegLowStd'
-    'BPSegBgFactor'
-    'BPSegThreshold'
-    'SegTopHatRadius'},...
-    'Grids', {[], [], [], [], [], 1:100});
-
-optimizer.Optimize_coordinatedescent('MaxIter', 25)
+for i = 1:length(seqDirs)
+    seqPath = fullfile(exPath, seqDirs{i});
+    fprintf('Optimizing seqmentation for %s\n', seqDirs{i})
+    
+    optimizer = SEGOptimizerSeq(seqPath,...
+        {'BPSegHighStd'
+        'BPSegLowStd'
+        'BPSegBgFactor'
+        'BPSegThreshold'});
+    
+    optimizer.Optimize_coordinatedescent('MaxIter', 25)
+end
