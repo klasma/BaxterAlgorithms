ca1c481f21b988169663c12ad08dc74de231452f
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2019-03-01 19:28:18 +0100

    Generated bat files for CSC 2019.
    Signed-off-by: Klas Magnusson <klas.magnusson@raysearchlabs.com>

---
+++ Fluo-C2DL-MSC-02.bat
@@ -0,0 +1,5 @@
+@echo off
+
+REM Prerequisities: MATLAB 2018b (x64)
+
+matlab -wait -r "RunBaxterAlgorithms_ISBI_2019('Fluo-C2DL-MSC', '02')"
---
+++ Fluo-C3DH-A549-SIM-01.bat
@@ -0,0 +1,5 @@
+@echo off
+
+REM Prerequisities: MATLAB 2018b (x64)
+
+matlab -wait -r "RunBaxterAlgorithms_ISBI_2019('Fluo-C3DH-A549-SIM', '01')"
