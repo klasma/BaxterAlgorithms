0de288216427addd7554a0a4f261912dedf74949
Klas Magnusson <klasmagnus@gmail.com>
2020-02-28 19:47:17 +0100

    Optimized the segmentation parameters for the MSC datasets.
    The the optimization performed both segmentation and tracking. This was
    done as the final result depends a lot on how watershed fragments of
    background objects are incorporated into the cells. The mean of SEG and TRA
    was used as utility function. The results did not look very promising on
    the challenge dataset, so this commit will be reverted.
    
    Signed-off-by: Klas Magnusson <klasmagnus@gmail.com>

---
+++ CSC_2020/TrackingOptimization/RunOptimizers_MSC01.m
@@ -0,0 +1,20 @@
+% Set SegTopHatRadius to 300 and started optimizing from the parameters
+% in CTC 2019.
+
+subdirs = textscan(genpath(fileparts(fileparts(fileparts(mfilename('fullpath'))))), '%s','delimiter', pathsep);
+addpath(subdirs{1}{:});
+
+basePath = 'C:\CTC2020\Training';
+seqPath = fullfile(basePath, 'Fluo-C2DL-MSC', 'Fluo-C2DL-MSC_01');
+
+MSC_optimizer1 = TRAOptimizerSeq(seqPath,...
+    {'SegTopHatRadius'
+    'BPSegThreshold'
+    'BPSegHighStd'
+    'BPSegLowStd'
+    'BPSegBgFactor'},...
+    'Grids', {1:5000 [] [] [] []},...
+    'SegmentationCores', 8,...
+    'Plot', true);
+
+MSC_optimizer1.Optimize_coordinatedescent('MaxIter', 25);
