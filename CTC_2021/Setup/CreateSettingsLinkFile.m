160395b6993f245fdec2c9a4568014c8a51c485b
Klas Magnusson <klasmagnus@gmail.com>
2021-02-21 23:04:40 +0100

    Updated the starting points for the segmentation optimization.
    I updated the clean settings that are used as starting points for the
    segmentation optimization. I changed the settings for the bandpass
    segmentation so that it gives smoother contours. This seems to work a
    little better for most image sequences. I also added a watershed transform
    based on the bandpass filtered image and a second watershed based on the
    region shape. Both watersheds use HMax transforms to avoid
    over-segmentation. I changed SegWSmooth from 7 to 0, as that parameter is
    used on few CTC datasets. I also changed the minimum area from 40 to 0 to
    avoid removing small cells. I added the background subtraction for MuSC and
    HSC as this will make the performance evaluation more meaningful. Now, the
    sequence names do not include the experiment names, for consistency with
    the other settings files.
    
    Signed-off-by: Klas Magnusson <klasmagnus@gmail.com>

---
+++ CTC_2021/Setup/CreateSettingsLinkFile.m
@@ -0,0 +1,21 @@
+function CreateSettingsLinkFile(aExPath, aTrainingOrChallenge, aSuffix)
+
+sett{1,1} = 'file';
+sett{1,2} = 'csv-file';
+sett{1,3} = 'fileLink';
+
+seqDirs = GetSeqDirs(aExPath);
+for s = 1:length(seqDirs)
+    settingsFileName = sprintf('Settings_ISBI_2021_%s_%s-%s%s.csv',...
+        aTrainingOrChallenge, FileEnd(aExPath), seqDirs{s}(end-1:end), aSuffix);
+    
+    % Values.
+    sett{s+1,1} = seqDirs{s};
+    sett{s+1,2} = settingsFileName;
+    sett{s+1,3} = seqDirs{s}(end-1:end);
+end
+
+% Save the link file.
+WriteSettings(fullfile(aExPath, sprintf('SettingsLinks%s.csv', aSuffix)), sett,...
+    'Transpose', false);
+end
--- CTC_2021/Setup/createCleanSettings.m
+++ CTC_2021/Setup/createCleanSettings.m
@@ -3,7 +3,35 @@
 
 dataSetFolder = 'C:\CTC2021\Training';
 
-settingsToKeep = {'numZ' 'zStacked' 'bits' 'voxelHeight' 'channelMin' 'channelMax'};
+settingsToKeep = {
+    'numZ'
+    'zStacked'
+    'bits'
+    'voxelHeight'
+    'channelMin'
+    'channelMax'
+    };
+bgSubSettings = {
+    'SegBgSubAlgorithm'
+    'SegMediaChanges'
+    'SegBgSubAtten'
+    };
+bgSubEx = {
+    'BF-C2DL-HSC'
+    'BF-C2DL-MuSC'
+    };
+defaults = {...
+    'SegMinArea', '0',...
+    'BPSegHighStd', '10',...
+    'BPSegLowStd', '3',...
+    'BPSegThreshold', '0.01',...
+    'SegWatershed', 'intermediate'...
+    'SegWSmooth', '0',...
+    'SegWHMax', '0.01',...
+    'SegWatershed2', 'shape'...
+    'SegWSmooth2', '0',...
+    'SegWHMax2', '2',...
+    };
 
 currentPath = fileparts(mfilename('fullpath'));
 newSettingsPath = fullfile(currentPath, '..', '..', 'Files', 'Settings', 'CTC2021_clean');
@@ -16,17 +44,27 @@ for e = 1:length(exDirs)
     
     for s = 1:length(seqDirs)
         seqDir = seqDirs{s};
+        num = seqDir(end-1:end);
         settings = ReadSettings(exPath, seqDir);
-        settings_clean = {'file'; seqDir};
+        settings_clean = {'file'; num};
         fprintf('Processing %s\n', seqDir)
         for i = 1:length(settingsToKeep)
             value = GetSeqSettings(settings, seqDir, settingsToKeep{i});
-            settings_clean = SetSeqSettings(settings_clean, seqDir, settingsToKeep{i}, value);
+            settings_clean = SetSeqSettings(settings_clean, num, settingsToKeep{i}, value);
         end
+        if any(strcmpi(bgSubEx, exDirs{e}))
+            for i = 1:length(bgSubSettings)
+                value = GetSeqSettings(settings, seqDir, bgSubSettings{i});
+                settings_clean = SetSeqSettings(settings_clean, num, bgSubSettings{i}, value);
+            end
+        end
+        settings_clean = SetSeqSettings(settings_clean, num, defaults{:});
         settingsFileName = sprintf('Settings_ISBI_2021_%s_%s-%s_clean.csv',...
-                trainingOrChallenge{d}, exDirs{e}, seqDir(end-1:end));
+                'Training', exDirs{e}, num);
         WriteSettings(fullfile(newSettingsPath, settingsFileName), settings_clean)
     end
+    
+    CreateSettingsLinkFile(exPath, 'Training', '_clean')
 end
 
 fprintf('Done creating clean settings files.\n')
