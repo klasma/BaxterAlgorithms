408be04de495019cfa96824d7c8b2943998f8735
Klas Magnusson <klas.magnusson@raysearchlabs.com>
2021-03-01 11:11:54 +0100

    Optimized Huh7 segmentation with SEG as utility function.
    Parts of the background were segmented as large cells. That was probably
    due to the penalty on false positives in the 0.9*SEG+0.1*DET utility
    function that was used before. SEGMinArea was 100 and SegMinSumIntensity
    was 5 throughout the optimization. These parameters were not optimzed.

---
+++ CTC_2021/optimizeSegmentationHuh7.m
@@ -0,0 +1,76 @@
+% Optimize segmentation on the primary track datasets for CTC 2021.
+
+subdirs = textscan(genpath(fileparts(fileparts(mfilename('fullpath')))), '%s','delimiter', pathsep);
+addpath(subdirs{1}{:});
+
+% % Settings to test on.
+% basePath = 'C:\CTC2021\Training';
+% exDirs = {'Fluo-C2DL-MSC'};
+% maxIter = 25;
+% settingsToOptimize = {
+%     'BPSegThreshold'
+%     };
+% overWriteOldOptimizers = false;
+% optimizerName = 'TestOptimizer.mat';
+
+% Real settings.
+basePath = 'C:\CTC2021\Training';
+% The fluorescence experiments are ordered so that the fastest ones are
+% processed first.
+exDirs = {
+    'Fluo-C2DL-Huh7'
+    };
+maxIter = 25;
+settingsToOptimize = {
+    'BPSegHighStd'
+    'BPSegLowStd'
+    'BPSegBgFactor'
+    'BPSegThreshold'
+    'SegClipping'
+    'SegWHMax'
+    'SegWHMax2'
+    };
+overWriteOldOptimizers = false;
+optimizerName = 'Huh7OptimizerCTC2021_SEG.mat';
+
+exPaths = fullfile(basePath, exDirs);
+
+allSettings = AllSettings();
+
+for i = 1:length(exPaths)
+    fprintf('Processing experiment %d / %d %s\n', i, length(exDirs), exDirs{i})
+    exPath = exPaths{i};
+    seqDirs = GetSeqDirs(exPath);
+    seqPaths = fullfile(exPath, seqDirs);
+    
+    % Read initial settings which only have information about the images.
+    initialImData = [];
+    for j = 1:length(seqDirs)
+        seqPath = fullfile(exPaths{i}, seqDirs{j});
+        settingsPath = fullfile(exPath, 'SettingsLinks_clean.csv');
+        imData = ImageData(seqPath, 'SettingsFile', settingsPath);
+        initialImData = [initialImData; imData];
+    end
+    
+    % Specify where the optimized settings should be saved.
+    optimizedSettingsPaths = cell(size(seqDirs));
+    for j = 1:length(seqDirs)
+        optimizedSettingsPaths{j} = fullfile(exPath, 'SettingsLinks_trained_on_GT.csv');
+    end
+    
+    % Specify where the segmentation optimizer should be saved.
+    optimizerSavePath = fullfile(exPath, 'Analysis', 'SegmentationOptimizers', optimizerName);
+    if ~overWriteOldOptimizers
+        assert(~exist(optimizerSavePath, 'file'),...
+            'The optimizer ''%s'' already exists.', optimizerSavePath)
+    end
+    
+    optimizer = SEGOptimizerEx(seqPaths, settingsToOptimize,...
+        'SavePaths', optimizedSettingsPaths,...
+        'OptimizerSavePath', optimizerSavePath,...
+        'InitialImData', initialImData,...
+        'ScoringFunction', 'SEG',...
+        'Plot', true);
+    
+    optimizer.Optimize_coordinatedescent('MaxIter', maxIter)
+end
