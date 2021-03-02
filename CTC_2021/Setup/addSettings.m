019ab3fb8ac4802ef1cc31154852de763e7d8bfc
Klas Magnusson <klasmagnus@gmail.com>
2021-03-02 08:27:29 +0100

    Set TrackSaveMat to 0 for ST.

--- CTC_2021/Setup/addSettings.m
+++ CTC_2021/Setup/addSettings.m
@@ -5,38 +5,44 @@ suffix = '_trained_on_ST';
 
 dataSetFolder = ['C:\CTC2021\' trainingOrChallenge];
 
+defaults = {
+    'TrackSaveMat', '0',...
+    };
+exDirs = {
+    'BF-C2DL-MuSC'
+    'BF-C2DL-HSC'
+    'Fluo-N3DH-CE'
+    };
+
 % defaults = {
-%     'TrackSaveMat', '0',...
+%     'TrackSaveFPAsCells', '1',...
+%     'TrackPAppear', '1E-3',...
+%     'TrackPDisappear', '1E-3',...
+%     'TrackXSpeedStd', '15',...
+%     'TrackZSpeedStd', '15',...
 %     'TrackSaveCTC', '1',...
 %     };
-
-defaults = {
-    'TrackSaveFPAsCells', '1',...
-    'TrackPAppear', '1E-3',...
-    'TrackPDisappear', '1E-3',...
-    'TrackXSpeedStd', '15',...
-    'TrackZSpeedStd', '15',...
-    'TrackSaveCTC', '1',...
-    };
+% 
+% exDirs = {
+%     'Fluo-C2DL-MSC'
+%     'Fluo-N2DH-GOWT1'
+%     'Fluo-C3DH-A549'
+%     'Fluo-C3DL-MDA231'
+%     'Fluo-N2DL-HeLa'
+%     'Fluo-N3DH-CHO'
+%     'PhC-C2DL-PSC'
+%     'PhC-C2DH-U373'
+%     'DIC-C2DH-HeLa'
+%     'BF-C2DL-MuSC'
+%     'BF-C2DL-HSC'
+%     'Fluo-N3DH-CE'
+%     'Fluo-C3DH-H157'
+%     };
 
 currentPath = fileparts(mfilename('fullpath'));
 newSettingsPath = fullfile(currentPath, '..', '..', 'Files', 'Settings', ['CTC2021' suffix]);
 
-exDirs = {
-    'Fluo-C2DL-MSC'
-    'Fluo-N2DH-GOWT1'
-    'Fluo-C3DH-A549'
-    'Fluo-C3DL-MDA231'
-    'Fluo-N2DL-HeLa'
-    'Fluo-N3DH-CHO'
-    'PhC-C2DL-PSC'
-    'PhC-C2DH-U373'
-    'DIC-C2DH-HeLa'
-    'BF-C2DL-MuSC'
-    'BF-C2DL-HSC'
-    'Fluo-N3DH-CE'
-    'Fluo-C3DH-H157'
-    };
+
 for e = 1:length(exDirs)
     exPath = fullfile(dataSetFolder, exDirs{e});
     seqDirs = GetSeqDirs(exPath);
