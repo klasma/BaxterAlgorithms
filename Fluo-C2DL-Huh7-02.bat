df54af27e09b9d4391db6bb2de91ad9c0d918cde
Klas Magnusson <klasmagnus@gmail.com>
2022-01-13 01:09:58 +0100

    Added bat-files for Huh7 and TRIF.
    For some reason, these bat-files were not present in the last submission. I
    probably removed all bat-files and then forgot to generate the ones for the
    secondary track.
    
    Signed-off-by: Klas Magnusson <klasmagnus@gmail.com>

---
+++ Fluo-C2DL-Huh7-02.bat
@@ -0,0 +1,5 @@
+@echo off
+
+REM Prerequisities: MATLAB 2019b (x64) including toolboxes for Image Processing, Optimization, Parallel Computing, and Statistics and Machine Learning
+
+matlab -wait -r "RunBaxterAlgorithms_ISBI_2021('Fluo-C2DL-Huh7', '02', 'Settings_ISBI_2021_Challenge_Fluo-C2DL-Huh7-02.csv', '')"
